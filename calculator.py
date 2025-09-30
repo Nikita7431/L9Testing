200614d8b1eae091a7e8b1d7d1001cb1730743e3
Nikita <yudin.nd@mail.ru>
2025-09-30 16:49:27 +0300

    L9

---
+++ calculator.py
@@ -0,0 +1,21 @@
+
+class Calculator:
+    
+    def __init__(self):
+        pass
+    
+    def add(self,a,b):
+        return a+b
+    
+    def divide(self,a,b):
+        if b == 0:
+            raise ZeroDivisionError
+        return a/b
+    
+    def is_prime_number(self, n):
+        if n < 2:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
